0080a8ae14810e347158187fef6ac41e0174f6ba
Michael Kreis <michi.kreis@gmail.com>
2012-05-14 19:06:41 +0200

    matlab improvements

---
+++ matlab/HilarisClient.m
@@ -0,0 +1,65 @@
+classdef HilarisClient
+    %HilarisClient a matlab wrapper for the java class StreamClient
+    %   as matlab and java uses different image formats in this class image
+    %   formats are converted. only color (BGR Halfsize) and grey (GREY
+    %   Halfsize) are supported yet
+    
+    properties
+        path = '';
+        port = 0;
+        client;
+        connected = false;
+    end
+    
+    methods
+        function hc = HilarisClient(path, port)
+           hc.path = path;
+           hc.port = port;
+           hc.client = ch.zhaw.hilaris.StreamClient;
+        end
+        
+        function connect(obj)
+          obj.client.connect(obj.path, obj.port);
+        end
+        
+        function disconnect(obj)
+            obj.connected = obj.client.disconnect();
+        end
+        
+        function setImageType(obj, type, width, height)
+           if (strcmp(type,'grey'))
+              obj.client.setImageType(javaMethod('valueOf', 'ch.zhaw.hilaris.StreamClient$ImageType', 'GREY_HALFSIZE'), width, height);
+           elseif(strcmp(type,'color'))
+               obj.client.setImageType(javaMethod('valueOf', 'ch.zhaw.hilaris.StreamClient$ImageType', 'BGR_HALFSIZE'), width, height);
+           else
+               obj.client.setImageType(javaMethod('valueOf', 'ch.zhaw.hilaris.StreamClient$ImageType', 'GREY_HALFSIZE'), width, height);
+           end
+        end
+        
+        function img = getImageData(obj)
+            javaImg = obj.client.getImage();
+            
+            H=javaImg.getHeight;
+            W=javaImg.getWidth;
+
+            % repackage as an array (MATLAB image format)
+            if (javaImg.getType == java.awt.image.BufferedImage.TYPE_BYTE_GRAY)
+                img = uint8(zeros([H,W]));
+                pixelsData = uint8(javaImg.getData.getPixels(0,0,W,H,[]));
+                for i = 1 : H
+                    base = (i-1)*W + 1;
+                    img(i,1:W) = pixelsData(base:(base+W-1));
+                end
+            elseif (javaImg.getType == java.awt.image.BufferedImage.TYPE_3BYTE_BGR)
+                img = uint8(zeros([H,W,3]));
+                pixelsData = uint8(javaImg.getData.getPixels(0,0,W,H,[]));
+                for i = 1 : H
+                    base = (i-1)*W*3+1;
+                    img(i,1:W,:) = deal(reshape(pixelsData(base:(base+3*W-1)),3,W)');
+                end
+            end
+        end
+    end
+    
+end
+
--- matlab/showImage.m
+++ matlab/showImage.m
@@ -1,6 +1,9 @@
 javaaddpath('/home/km/Desktop/streamclient.jar');
 client = ch.zhaw.hilaris.StreamClient();
-client.connect('stud-kreismic-1', 9003);
+client.connect('192.168.1.10', 9003);
+
+thres = 10;
+run = 1;
 
 javaImage = client.getImage();
 
@@ -8,14 +11,52 @@ javaImage = client.getImage();
 H=javaImage.getHeight;
 W=javaImage.getWidth;
 
-% repackage as a 3D array (MATLAB image format)
-B = uint8(zeros([H,W]));
+bg = uint8(zeros([H,W]));
 pixelsData = uint8(javaImage.getData.getPixels(0,0,W,H,[]));
 for i = 1 : H
     base = (i-1)*W + 1;
-    B(i,1:W) = pixelsData(base:(base+W-1));
+    bg(i,1:W) = pixelsData(base:(base+W-1));
 end
 
-% display image
-imshow(B);
+while run
+    javaImage = client.getImage();
+
+    % get image properties
+    H=javaImage.getHeight;
+    W=javaImage.getWidth;
+
+    % repackage as an array (MATLAB image format)
+    B = uint8(zeros([H,W]));
+    pixelsData = uint8(javaImage.getData.getPixels(0,0,W,H,[]));
+    for i = 1 : H
+        base = (i-1)*W + 1;
+        B(i,1:W) = pixelsData(base:(base+W-1));
+    end
+    
+    diff = uint8(zeros([H,W]));
+    
+    for j=1:H
+        for k=1:W
+            if(B(j,k)+thres>bg(j,k))
+                bg(j,k) = bg(j,k)+1;
+            elseif (B(j,k)-thres<bg(j,k))
+                bg(j,k) = bg(j,k)-1;
+            end
+            
+
+        end
+    end
+    
+    diff = imabsdiff(bg, B);
+    % display image
+    %imshow(imsubtract(B, bg));
+    fig = figure(1);
+    subplot(2,2,1),imshow(B) 
+    subplot(2,2,2),imshow(bg)
+    subplot(2,2,3),imshow(diff) 
+    sedisk = strel('disk', 1);
+    subplot(2,2,4),imshow(imfill(imopen(im2bw(diff, 0.1),sedisk), 'holes')) 
+    uicontrol('Style', 'pushbutton', 'String', 'stop','Position', [20 20 50 20], 'Callback', 'run=0;close(fig)');
+    drawnow;
+end
 client.disconnect();
